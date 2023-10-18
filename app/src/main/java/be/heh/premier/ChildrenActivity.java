ee349d6eb08ccd608a3808d7b3a244793f7a443d
KouptchN <kouptchinskyng@gmail.com>
2023-10-18 11:57:03 +0200

    file activity start

--- app/src/main/java/be/heh/premier/ChildrenActivity.java
+++ app/src/main/java/be/heh/premier/ChildrenActivity.java
@@ -11,17 +11,20 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ChildrenActivity extends AppCompatActivity {
     EditText et_children_login;
     EditText et_children_pwd;
-    SharedPreferences prefs_datas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_children);
         et_children_login = findViewById(R.id.et_children_login);
         et_children_pwd = findViewById(R.id.et_children_pwd);
-        prefs_datas= PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
     }
     public void onChildrenClickManager(View v) {
 // Récupérer la vue et accéder au bouton
@@ -31,10 +34,20 @@ public class ChildrenActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
             else {
-                SharedPreferences.Editor editeur_datas=prefs_datas.edit();
-                editeur_datas.putString("login",et_children_login.getText().toString());
-                editeur_datas.putString("pwd",et_children_pwd.getText().toString());
-                editeur_datas.commit();
+                String str = et_children_login.getText().toString() + "#" +
+                        et_children_pwd.getText().toString() + "#";
+                Toast.makeText(getApplicationContext(),str,Toast.LENGTH_LONG).show();
+                try {
+                    FileOutputStream ous = openFileOutput("monfichier.txt", MODE_APPEND);
+                    byte[] tab;
+                    tab = str.toString().getBytes();
+                    ous.write(tab);
+                    ous.close();
+                }catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 finish();
