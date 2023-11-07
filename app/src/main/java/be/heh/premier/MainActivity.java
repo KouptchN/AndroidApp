b58b54cfcd581858481bb94d64bbe498ecd495c0
KouptchN <kouptchinskyng@gmail.com>
2023-11-07 11:30:14 +0100

    notif (dont work)

--- app/src/main/java/be/heh/premier/MainActivity.java
+++ app/src/main/java/be/heh/premier/MainActivity.java
@@ -4,21 +4,29 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    private static final int NOTIFY_ID=1234;
+    NotificationManager noma =null;
+    int cpt=0;
     SharedPreferences prefs_datas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         prefs_datas = PreferenceManager.getDefaultSharedPreferences(getApplication());
+        noma=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         if(!prefs_datas.getAll().isEmpty())
         {
             Toast.makeText(getApplicationContext(), "votre login est : "
@@ -36,20 +44,33 @@ public class MainActivity extends Activity {
                             .setMessage("Voulez-vous afficher l'activité Children ?")
                             .setCancelable(false)
                             .setIcon(R.mipmap.ic_launcher)
-                            .setPositiveButton("Oui", new DialogInterface.OnClickListener(){
-                                @Override
-                                public void onClick(DialogInterface dialog, int which){
-                                    Intent intent = new Intent(getApplicationContext(), ChildrenActivity.class);
-                                    startActivity(intent);
-                                }
+                            .setPositiveButton("Oui", (dialog, which) -> {
+                                notifier();
+                                /*Intent intent = new Intent(getApplicationContext(), ChildrenActivity.class);
+                                startActivity(intent);*/
                             })
-                            .setNegativeButton("Annuler",new DialogInterface.OnClickListener(){
-                                @Override
-                                public void onClick(DialogInterface dialog, int which){
-                                    dialog.cancel();
-                                }
-                            })
+                            .setNegativeButton("Annuler", (dialog, which) -> dialog.cancel())
                             .create().show();
         }
+    }
+    public void notifier(){
+        PendingIntent pi = PendingIntent.getActivity(this,0,new Intent(this,ChildrenActivity.class ), PendingIntent.FLAG_IMMUTABLE);
+//count = Integer.getInteger(BatteryManager.EXTRA_LEVEL);
+        BatteryManager bm = (BatteryManager)getSystemService(BATTERY_SERVICE);
+        int batLevel = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        Notification.Builder notification = new Notification.Builder(this)
+                .setAutoCancel(true)
+                .setContentTitle("Notification !!!")
+                .setContentText("L'activité Children est disponible ! \n Niveau de batterie : "+batLevel+" \n ID : ")
+                .setContentIntent(pi)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setWhen(System.currentTimeMillis())
+                .setNumber(++cpt);
+        Notification notif = notification.build();
+        noma.notify(NOTIFY_ID,notif);
+    }
+    public void arretNotifier(){
+        noma.cancel(NOTIFY_ID);
     }
 }
